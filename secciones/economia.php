5c87da409dbc20123ef2dd2a4fee2f378b3ee60b
Robert Anthony Obeso Olivos <119701832+anthonyobesouns@users.noreply.github.com>
2024-01-09 22:19:48 -0500

    Update economia.php

--- secciones/economia.php
+++ secciones/economia.php
@@ -4,8 +4,10 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $economia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+
+$seccion = [
+    "titulo" => "Perspectivas Económicas Globales",
+    "autor" => "Carlos Gutiérrez",
+    "contenido" => "Analizamos las tendencias económicas actuales y su impacto en los mercados financieros. Desde noticias empresariales hasta desarrollos macroeconómicos, mantente informado sobre el panorama económico global y local.",
 ];
 ?>
