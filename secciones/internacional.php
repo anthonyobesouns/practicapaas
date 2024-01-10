eabd5034855321b9e62447d0fd7b43ea5ec7918a
Robert Anthony Obeso Olivos <119701832+anthonyobesouns@users.noreply.github.com>
2024-01-09 22:22:07 -0500

    Update internacional.php

--- secciones/internacional.php
+++ secciones/internacional.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $internacional = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Noticias Internacionales: Explorando el Mundo",
+    "autor" => "Alejandro Ramírez",
+    "contenido" => "Mantente al día con las últimas noticias y desarrollos a nivel internacional. Desde eventos políticos y económicos hasta cambios culturales, exploramos el panorama global para brindarte una perspectiva completa de lo que está sucediendo en el mundo.",
 ];
 ?>
