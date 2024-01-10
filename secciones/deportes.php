5b9a692cea4bdd00e69701ab6c236673a8185097
Robert Anthony Obeso Olivos <119701832+anthonyobesouns@users.noreply.github.com>
2024-01-09 22:18:32 -0500

    Update deportes.php

--- secciones/deportes.php
+++ secciones/deportes.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $deportes = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Noticias Deportivas Destacadas",
+    "autor" => "Laura Martínez",
+    "contenido" => "Descubre las últimas noticias y eventos emocionantes en el mundo del deporte. Desde resultados de partidos hasta historias inspiradoras de atletas, mantenemos actualizados a nuestros lectores con lo mejor del deporte.",
 ];
 ?>
