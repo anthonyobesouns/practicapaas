960bd409c0cb12f54f8029cdba46160206028120
Robert Anthony Obeso Olivos <119701832+anthonyobesouns@users.noreply.github.com>
2024-01-09 22:21:44 -0500

    Update git.php

--- secciones/git.php
+++ secciones/git.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $git = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Dominando Git: Control de Versiones en Desarrollo",
+    "autor" => "Carlos Hernández",
+    "contenido" => "Explora las mejores prácticas en el uso de Git para el control de versiones en proyectos de desarrollo de software. Desde la creación de repositorios hasta estrategias de ramificación, aprende cómo optimizar tu flujo de trabajo con una de las herramientas más esenciales en el desarrollo colaborativo.",
 ];
 ?>
