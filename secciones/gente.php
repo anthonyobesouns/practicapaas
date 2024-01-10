0d0535c223fe7ef63c8841085409c3ddf038d132
Robert Anthony Obeso Olivos <119701832+anthonyobesouns@users.noreply.github.com>
2024-01-09 22:21:21 -0500

    Update gente.php

--- secciones/gente.php
+++ secciones/gente.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $gente = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Historias Fascinantes de Personas Inspiradoras",
+    "autor" => "Elena Sánchez",
+    "contenido" => "Descubre las historias de individuos excepcionales que han dejado una huella en la sociedad. Desde líderes comunitarios hasta personas que han superado desafíos extraordinarios, exploramos las experiencias de gente inspiradora que ha marcado la diferencia.",
 ];
 ?>
