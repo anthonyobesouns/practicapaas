caec7177c5128d1060e21ab6d0de21a0da78720d
Robert Anthony Obeso Olivos <119701832+anthonyobesouns@users.noreply.github.com>
2024-01-09 22:17:37 -0500

    Update ciencia.php

--- secciones/ciencia.php
+++ secciones/ciencia.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $ciencia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "Descubrimiento Revolucionario en la Ciencia",
+    "autor" => "Dr. Juan Pérez",
+    "resumen" => "Un equipo de científicos liderado por el Dr. Juan Pérez ha realizado un descubrimiento sorprendente que podría cambiar nuestra comprensión actual de XXX. El estudio revela XXX, abriendo nuevas posibilidades en la investigación científica y aplicaciones prácticas. Este avance tiene el potencial de impactar significativamente en campos como XXX, brindando nuevas perspectivas para el futuro de la ciencia y la tecnología.",
 ];
 ?>
