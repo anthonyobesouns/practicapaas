99e94754036c228d7ed5d248f93191b453c5ff97
Robert Anthony Obeso Olivos <119701832+anthonyobesouns@users.noreply.github.com>
2024-01-09 22:19:24 -0500

    Update cultura.php

--- secciones/cultura.php
+++ secciones/cultura.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $cultura = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Explorando la Diversidad Cultural",
+    "autor" => "María Rodríguez",
+    "contenido" => "Sumérgete en las riquezas culturales de todo el mundo. Desde expresiones artísticas hasta tradiciones culinarias, descubre la fascinante diversidad que enriquece nuestra sociedad y nos conecta a través de experiencias únicas.",
 ];
 ?>
