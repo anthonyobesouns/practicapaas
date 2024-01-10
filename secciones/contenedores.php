60c2197e420c8f09556e288b6c95aa4e40f3beb4
Robert Anthony Obeso Olivos <119701832+anthonyobesouns@users.noreply.github.com>
2024-01-09 22:17:21 -0500

    Update contenedores.php

--- secciones/contenedores.php
+++ secciones/contenedores.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $contenedores = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Avances en Tecnología de Contenedores",
+    "autor" => "Dr. Ana García",
+    "resumen" => "Exploramos los últimos avances y tendencias en la tecnología de contenedores. Desde Docker hasta Kubernetes, descubre cómo estas soluciones están transformando el desarrollo y despliegue de aplicaciones.",
 ];
 ?>
